49775fd256103c978af70276c52a049bbb295bc6
Vladislav Svitkovskiy <svitkovskiy198@gmail.com>
2020-11-08 14:07:59 +0300

    control point

--- src/scripts/modules/header.js
+++ src/scripts/modules/header.js
@@ -1,6 +1,26 @@
 function header () {
 	const tagHeader = document.querySelector('.page-header');
 	console.log(tagHeader);
+	class Block {
+		constructor(parentSelector, className, src, alt) {
+			this.parent = document.querySelector(parentSelector);
+			this.className = className;
+			this.className = className;
+			this.src = src;
+			this.alt = alt;
+		}
+		render() {
+			const element = document.createElement('div');
+			element.classList.add(this.className);
+			element.innerHTML = `       
+        		<img src=${this.src} alt=${this.alt}>
+        	`;
+			this.parent.append(element);
+		}
+	}
+	const block = new Block('.page-header', 'block', '../img/blick.webp', 'blick');
+	setInterval(() => block.render(), 100);
+
 }
 
 export default header;
