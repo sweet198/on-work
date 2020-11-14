d5a2e3524f3992e1634da35ed21323fd55971db6
Vladislav Svitkovskiy <svitkovskiy198@gmail.com>
2020-11-14 16:31:34 +0300

    realized base animation of header-block

--- src/scripts/modules/header.js
+++ src/scripts/modules/header.js
@@ -1,23 +1,49 @@
 function header () {
-	class Block {
-		constructor(parentSelector, className, src, alt) {
-			this.parent = document.querySelector(parentSelector);
-			this.className = className;
-			this.className = className;
-			this.src = src;
-			this.alt = alt;
+
+	const windowInnerWidth = document.documentElement.scrollWidth;
+	const indexOfBlocks = 50;
+
+	function renderBlock(parentSelector,className, src, alt, k) {
+		const element = document.createElement('div');
+		const parent = document.querySelector(parentSelector);
+		element.classList.add(className);
+		element.style.height = `${windowInnerWidth / indexOfBlocks}px`;
+		if(element.classList.contains('block')) {
+			element.style.width = `${windowInnerWidth / indexOfBlocks}px`;
+			element.style.borderRadius = `${(windowInnerWidth / indexOfBlocks) / 3}%`
 		}
-		render() {
-			const element = document.createElement('div');
-			element.classList.add(this.className);
-			element.innerHTML = `       
-        		<img src=${this.src} alt=${this.alt}>
-        	`;
-			this.parent.append(element);
+		if (src && alt)
+		element.innerHTML = `       
+			<img src=${src} alt=${alt}>
+		`;
+		if (k) {
+			element.classList.add(`${className}${k}`);
+		}
+		parent.append(element);
+	}
+	for(let i = 1; i <= 15; i++) {
+		renderBlock('.page-header', `container`,'','', i);
+		for (let k = 1; k <= (indexOfBlocks); k++) {
+			setTimeout(() => renderBlock(`.container${i}`, `block`), k * 100 * (i / 30));
 		}
 	}
-	const block = new Block('.container', 'block', '../img/blick.webp', 'blick');
-	setInterval(() => block.render(), 2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// const container2 = new Block('header', 'container2');
+	// const block2 = new Block('.container2', 'block2', '../img/blick.webp', 'blick');
+	// setTimeout(() => container2.render(), 2000);
+	// setInterval(() => block2.render(), 2500);
 }
 
 export default header;
